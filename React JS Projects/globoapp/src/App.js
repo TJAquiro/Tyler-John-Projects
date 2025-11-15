b8f04c175770d3f7466f10a376ff3350d3df1ed3
TJAquiro <tjaquiro@gmail.com>
2025-11-15 10:42:13 -0500

    React Starter Project Created

---
+++ React JS Projects/globoapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Greet from './Components/Greet';
+import Welcome from './Components/Welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet></Greet>
+      <Welcome/>
+    </div>
+  );
+}
+
+export default App;
