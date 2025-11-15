8c9b8d71f43d3b9fabf858d6f2ad72819c8ad142
TJAquiro <tjaquiro@gmail.com>
2025-11-15 11:54:48 -0500

    Hello Fixed

--- React JS Projects/globoapp/src/App.js
+++ React JS Projects/globoapp/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Greet from './Components/Greet';
 import Welcome from './Components/Welcome';
-import Hello from './Components/hello';
+import Helloworld from './Components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <Greet></Greet>
       <Welcome/>
-      
+      <Helloworld></Helloworld>
+
     </div>
   );
 }
