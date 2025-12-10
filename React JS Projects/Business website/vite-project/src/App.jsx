7a5f0e7d2cf4c57758611535a80cbc4553945dee
TJAquiro <tjaquiro@gmail.com>
2025-12-09 23:33:57 -0500

    Improved UI

---
+++ React JS Projects/Business website/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import Banner from "./components/Banner"
+import './App.css'
+import HouseList from "./components/HouseList"
+
+
+function App() {
+  return (
+    <>
+      <Banner>Welcome to GloboCorp</Banner>
+      <HouseList/>
+    </>
+  )
+}
+
+export default App
